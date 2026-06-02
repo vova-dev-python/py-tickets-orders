0a54702d457c2123a0b34156927c07e4461e9543
Volodymyr Yalechko <Volodymyr.I.Yalechko@lpnu.ua>
2026-06-02 23:34:36 +0300

    Fix final flake8 line length

--- cinema/views.py
+++ cinema/views.py
@@ -41,7 +41,9 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.prefetch_related("genres", "actors").order_by("id")
+    queryset = Movie.objects.prefetch_related(
+        "genres", "actors"
+    ).order_by("id")
 
     def get_queryset(self):
         queryset = self.queryset
